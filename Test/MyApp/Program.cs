497f595890a94c246a5b31674d4b35931bdc3a15
echocslsk <CardioLeo@gmail.com>
2025-03-17 19:02:32 -0600

    adds Test folder and new dotnet app within it to the repo; first commit after finally getting C# to run; now need to make sure everything works within Visual Studio Code

---
+++ Test/MyApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C Sharp via Ubuntu 20.04!");
+// followed this link: https://terminalroot.com/how-to-install-csharp-on-ubuntu-and-getting-started/
