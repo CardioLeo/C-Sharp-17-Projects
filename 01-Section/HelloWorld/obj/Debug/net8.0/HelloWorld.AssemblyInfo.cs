f7763e2159823f93c4029ab6bf263501ae6b9fb6
echocslsk <CardioLeo@gmail.com>
2025-03-17 19:54:25 -0600

    updates all the subfolders after changes in previous commit

--- 01-Section/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ 01-Section/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9820c0cee3818fe540c9ba23a902a6549cfc9fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a104a7a258b0d6739cfc895b2437c725b4a057c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
