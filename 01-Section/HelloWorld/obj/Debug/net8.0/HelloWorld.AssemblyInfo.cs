ed2c724ec83a94ee1cf913e49d72979b90717c31
echocslsk <CardioLeo@gmail.com>
2025-03-17 21:55:05 -0600

    adds two WindowsForm directories, based on two different attempts to make windows forms work on Linux, with or without the full Visual Studio; also adds a note.txt file that shares the link used for take 2; but neither take was successful; this commit also updates the contents of HelloWorld directory, but none of them are files I manually changed

--- 01-Section/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ 01-Section/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a104a7a258b0d6739cfc895b2437c725b4a057c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7763e2159823f93c4029ab6bf263501ae6b9fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
