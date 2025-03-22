57be7211aff62af04fe6ecd2a3de41a006ac2fa9
echocslsk <CardioLeo@gmail.com>
2025-03-22 13:26:26 -0600

    brings the code roughly up to date with the end of section 02, mostly by adding about 4 lines of code and more comments to what I previously wrote; not really any significant changes

--- 02-C#-Basics/21-Type-Conversion.cs
+++ 02-C#-Basics/21-Type-Conversion.cs
@@ -9,8 +9,24 @@ namespace conversions
 
 		protected void okButton_Click(object sender, EventArgs e)
 		{
+			// from video 21
 			int i;
 			i = Convert.ToInt32("Hello World!");
+			// below begins coding along with video 22
+			double k = 3.5;
+			int j = (int)k; // this will truncate the value of k when saving into j
+			// resultLabel.Text = i; // use helper method to convert
+			resultLabel.Text = i.ToString(); // should output 3, truncated
+
+			// next he comments out all of the above
+			
+			string i2 = inputTextBox.Text;
+			int j2 = int.Parse(i); // will only work when the string looks exactly like a number!
+			// need rigorous testing and checks!
+			int k2 = j2 + 1; // misread the 1 as an i, and figured he meant i2
+			resultLabel.Text = k2.ToString();
+
+			// does he not use i2?
 		}
 	}
 }
