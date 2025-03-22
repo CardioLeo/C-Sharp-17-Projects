d8acfc20185bf6efc927a704480082c4bed566a4
echocslsk <CardioLeo@gmail.com>
2025-03-21 21:02:24 -0600

    renames directory by adding 01- at the beginning of it

---
+++ 02-C#-Basics/01-First-Console-App/HelloWorld.cs
@@ -0,0 +1,12 @@
+//
+// using vim instead of Windows notepad
+//
+
+public class HelloWorld
+{
+        public static void Main()
+        {
+                System.Console.WriteLine("Hello world!");
+                System.Console.ReadLine();
+        }
+}
