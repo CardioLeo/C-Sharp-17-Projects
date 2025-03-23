b03c1c826ee89594f9e651e747b3cdf5bce34cf5
echocslsk <CardioLeo@gmail.com>
2025-03-22 23:03:04 -0600

    adds project code as it stands after finishing video 5 of section 4;

---
+++ 04-Matching-Pairs-Project/05-Create-Matching-Symbols/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingPairsGame
+{
+    public partial class Form1: Form
+    {
+        // create random object to generate random shapes, or icons
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!",
+            "U", "U",
+            "g", "g",
+            "k", "k",
+            "R", "R",
+            "7", "7",
+            "#", "#",
+            "s", "s",
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
