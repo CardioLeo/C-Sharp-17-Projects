d944d0666e051bc848740039dcd70f7e6076b5ce
echocslsk <CardioLeo@gmail.com>
2025-03-23 00:22:13 -0600

    makes matched pairs visible, as per lectures 10 and 11 of section 04; brings code up to end of lecture 11; ready for number 12 now

--- 04-Matching-Pairs-Project/09-References-Timers-Matches/Form1.cs
+++ 04-Matching-Pairs-Project/09-References-Timers-Matches/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// my code here is based on the instructor's code, and I'm learning a lot;
+// but I also feel like this could be made to be more readable,
+// if nothing else by good use of comments
+
 namespace MatchingPairsGame
 {
     public partial class Form1: Form
@@ -54,16 +58,19 @@ namespace MatchingPairsGame
 
         private void label_click(object sender, EventArgs e)
         {
+            if (timer1.Enabled == true)
+            {
+                // timer starts running only after two non-matching icons shown to player
+                return; // ignore any clicks if timer is running
+            }
             // a stub, an empty method for the event handler
             Label clickedLabel = sender as Label;
             if (clickedLabel != null)
             {
-                //if (clickedLabel.ForeColor == Color.Black)
-                //return;
-                // I commented out the above two lines
-                // because I wasn't sure they actually did anything
-                // for him... not yet, anyways
-                // yeah; uncommented them and tested them and it doesn't change anything substantially
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                // okay; this actually is important! I was wrong
+                // it just didn't make any difference earlier
 
                 // clickedLabel.ForeColor = Color.Black;
                 // now he says we should comment this out
@@ -74,7 +81,35 @@ namespace MatchingPairsGame
                     firstClicked.ForeColor = Color.Black;
                     return;
                 }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                    // by returning here, it doesn't set the timer, and the timer doesn't call the tick (or reset) function
+                    // this means that the colors should stay black, and the icons should remain visible; smart
+                }
+
+                timer1.Start(); // will call timer1_Tick();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // I don't like this name, timer1_Tick, because it would make more sense
+            // as, say, "reset_seen_icons()" or something like that
+            // before any click, timer stops - he says
+            timer1.Stop();
+            // makes them invisible (but they're already invisible...?)
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            // resets variable again to be clicked on
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
