7332bed26481d6c98105eeb85f5d4dd6ba0e779b
echocslsk <CardioLeo@gmail.com>
2025-03-22 23:43:46 -0600

    copies all project files from folder for video 05 into new folder beginning with video 09; but as of right now it is purely a copy

---
+++ 04-Matching-Pairs-Project/09-References-Timers-Matches/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingPairsGame
+{
+    public partial class Form1: Form
+    {
+        // create random object to generate random shapes, or icons
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!",
+            "u", "u", // U and u are fine
+            "U", "U", // don't use lower case g, it's a huge square, too big
+            "k", "k",
+            "R", "R",
+            "7", "7",
+            "#", "#",
+            "s", "s",
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+            // a stub, an empty method for the event handler
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                //if (clickedLabel.ForeColor == Color.Black)
+                    //return;
+                    // I commented out the above two lines
+                    // because I wasn't sure they actually did anything
+                    // for him... not yet, anyways
+                clickedLabel.ForeColor = Color.Black;
+            }
+        }
+    }
+}
