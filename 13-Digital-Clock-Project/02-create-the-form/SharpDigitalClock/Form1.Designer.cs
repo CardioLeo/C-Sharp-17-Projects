c1323d08c0d2da8b77b44d1be8f728316bdffdf5
echocslsk <CardioLeo@gmail.com>
2025-05-10 13:27:57 -0600

    creates secondsLabel

--- 13-Digital-Clock-Project/02-create-the-form/SharpDigitalClock/Form1.Designer.cs
+++ 13-Digital-Clock-Project/02-create-the-form/SharpDigitalClock/Form1.Designer.cs
@@ -29,17 +29,28 @@
         private void InitializeComponent()
         {
             this.hourMinute = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // hourMinute
             // 
             this.hourMinute.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.hourMinute.ForeColor = System.Drawing.Color.DarkSlateBlue;
-            this.hourMinute.Location = new System.Drawing.Point(45, 38);
+            this.hourMinute.Location = new System.Drawing.Point(12, 9);
             this.hourMinute.Name = "hourMinute";
-            this.hourMinute.Size = new System.Drawing.Size(409, 173);
+            this.hourMinute.Size = new System.Drawing.Size(257, 121);
             this.hourMinute.TabIndex = 0;
             this.hourMinute.Text = "19:25";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.Font = new System.Drawing.Font("Bahnschrift", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.secondsLabel.Location = new System.Drawing.Point(235, 9);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(154, 121);
+            this.secondsLabel.TabIndex = 1;
+            this.secondsLabel.Text = "47";
             // 
             // Form1
             // 
@@ -47,7 +58,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.IndianRed;
-            this.ClientSize = new System.Drawing.Size(496, 261);
+            this.ClientSize = new System.Drawing.Size(528, 261);
+            this.Controls.Add(this.secondsLabel);
             this.Controls.Add(this.hourMinute);
             this.Name = "Form1";
             this.Text = "Sharp Digital Clock";
@@ -58,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Label hourMinute;
+        private System.Windows.Forms.Label secondsLabel;
     }
 }
 
