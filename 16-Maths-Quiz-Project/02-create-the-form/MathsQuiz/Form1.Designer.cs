b0c026df27f463251e8b28f17ce43b008f669af3
echocslsk <CardioLeo@gmail.com>
2025-05-13 22:48:45 -0600

    adds two labels for timer, changes color, size, and font; positions them where I want them

--- 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
+++ 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
@@ -28,7 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timeLeftTextBox = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("Bahnschrift", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.Bisque;
+            this.timeLabel.Location = new System.Drawing.Point(272, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 39);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "timer";
+            // 
+            // timeLeftTextBox
+            // 
+            this.timeLeftTextBox.AutoSize = true;
+            this.timeLeftTextBox.Font = new System.Drawing.Font("Bahnschrift", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftTextBox.ForeColor = System.Drawing.Color.Bisque;
+            this.timeLeftTextBox.Location = new System.Drawing.Point(161, 9);
+            this.timeLeftTextBox.Name = "timeLeftTextBox";
+            this.timeLeftTextBox.Size = new System.Drawing.Size(105, 27);
+            this.timeLeftTextBox.TabIndex = 1;
+            this.timeLeftTextBox.Text = "Time Left";
             // 
             // Form1
             // 
@@ -36,15 +60,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Salmon;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.timeLeftTextBox);
+            this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Sharp Maths Quiz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label timeLeftTextBox;
     }
 }
 
