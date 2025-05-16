891b86b31bb0952e560f80b6072cf738514e1dd6
echocslsk <CardioLeo@gmail.com>
2025-05-16 00:32:54 -0600

    adds the labels and numberic-up-down button for the addition controls, video 140

--- 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
+++ 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.timeLabel = new System.Windows.Forms.Label();
             this.timeLeftTextBox = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.plusSignLabel = new System.Windows.Forms.Label();
+            this.plusRightLabel = new System.Windows.Forms.Label();
+            this.plusEqualsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // timeLabel
@@ -54,18 +60,76 @@
             this.timeLeftTextBox.TabIndex = 1;
             this.timeLeftTextBox.Text = "Time Left";
             // 
+            // plusLeftLabel
+            // 
+            this.plusLeftLabel.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusLeftLabel.ForeColor = System.Drawing.Color.Bisque;
+            this.plusLeftLabel.Location = new System.Drawing.Point(39, 57);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusLeftLabel.TabIndex = 2;
+            this.plusLeftLabel.Text = "?";
+            this.plusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(315, 71);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(92, 30);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // plusSignLabel
+            // 
+            this.plusSignLabel.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusSignLabel.ForeColor = System.Drawing.Color.Bisque;
+            this.plusSignLabel.Location = new System.Drawing.Point(105, 57);
+            this.plusSignLabel.Name = "plusSignLabel";
+            this.plusSignLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusSignLabel.TabIndex = 7;
+            this.plusSignLabel.Text = "+";
+            this.plusSignLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusRightLabel
+            // 
+            this.plusRightLabel.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusRightLabel.ForeColor = System.Drawing.Color.Bisque;
+            this.plusRightLabel.Location = new System.Drawing.Point(171, 57);
+            this.plusRightLabel.Name = "plusRightLabel";
+            this.plusRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusRightLabel.TabIndex = 8;
+            this.plusRightLabel.Text = "?";
+            this.plusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusEqualsLabel
+            // 
+            this.plusEqualsLabel.Font = new System.Drawing.Font("Bahnschrift", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusEqualsLabel.ForeColor = System.Drawing.Color.Bisque;
+            this.plusEqualsLabel.Location = new System.Drawing.Point(234, 57);
+            this.plusEqualsLabel.Name = "plusEqualsLabel";
+            this.plusEqualsLabel.Size = new System.Drawing.Size(60, 50);
+            this.plusEqualsLabel.TabIndex = 9;
+            this.plusEqualsLabel.Text = "=";
+            this.plusEqualsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Salmon;
             this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.plusEqualsLabel);
+            this.Controls.Add(this.plusRightLabel);
+            this.Controls.Add(this.plusSignLabel);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.plusLeftLabel);
             this.Controls.Add(this.timeLeftTextBox);
             this.Controls.Add(this.timeLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Sharp Maths Quiz";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +139,11 @@
 
         private System.Windows.Forms.Label timeLabel;
         private System.Windows.Forms.Label timeLeftTextBox;
+        private System.Windows.Forms.Label plusLeftLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label plusSignLabel;
+        private System.Windows.Forms.Label plusRightLabel;
+        private System.Windows.Forms.Label plusEqualsLabel;
     }
 }
 
