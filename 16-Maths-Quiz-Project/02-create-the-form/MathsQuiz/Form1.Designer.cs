eb5d142a84c83c32f4eb2643059ca9e0a534296d
echocslsk <CardioLeo@gmail.com>
2025-05-18 00:08:22 -0600

    fleshes out the main logic for the timer, and how it displays both before and after the time has run out

--- 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
+++ 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.timeLabel = new System.Windows.Forms.Label();
             this.timeLeftTextBox = new System.Windows.Forms.Label();
             this.plusLeftLabel = new System.Windows.Forms.Label();
@@ -51,6 +52,7 @@
             this.quotientSelector = new System.Windows.Forms.NumericUpDown();
             this.divisionLeftLabel = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.sumSelector)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.differenceSelector)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productSelector)).BeginInit();
@@ -306,6 +308,12 @@
             this.startButton.TabIndex = 1;
             this.startButton.Text = "Start Quiz";
             this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -374,6 +382,7 @@
         private System.Windows.Forms.NumericUpDown quotientSelector;
         private System.Windows.Forms.Label divisionLeftLabel;
         private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.cs
+++ 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.cs
@@ -30,5 +30,35 @@ namespace MathsQuiz
         {
             InitializeComponent();
         }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (clock > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label
+                clock = clock - 1;
+                timeLabel.Text = clock + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer,
+                // show a MessageBox, and fill in the answers
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry...");
+                sumSelector.Value = addNum1 + addNum2;
+                // differenceSelector.Value = minusNum1 - minusNum2;
+                // productSelector.Value = timesNum1 * timesNum2;
+                // quotientSelector.Value = divisionNum1 / divisionNum2;
+                startButton.Enabled = true;
+            }
+        }
     }
 }
