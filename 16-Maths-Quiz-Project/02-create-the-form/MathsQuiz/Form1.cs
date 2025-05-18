0a973f6441bff45a769687eb1eafd6eec9c8b9a0
echocslsk <CardioLeo@gmail.com>
2025-05-17 23:57:04 -0600

    adds the startQuiz function; only in rudimentary form, setting two random numbers and changing the plusLeftLabel and plusRightLabel to those numbers; setting sumSelector.Value to zero

--- 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.cs
+++ 16-Maths-Quiz-Project/02-create-the-form/MathsQuiz/Form1.cs
@@ -15,6 +15,17 @@ namespace MathsQuiz
         Random randomizer = new Random();
         int addNum1;
         int addNum2;
+
+        public void startQuiz()
+        {
+            addNum1 = randomizer.Next(51);
+            addNum2 = randomizer.Next(51);
+            plusLeftLabel.Text = addNum1.ToString();
+            plusRightLabel.Text = addNum2.ToString();
+
+            sumSelector.Value = 0;
+
+        }
         public Form1()
         {
             InitializeComponent();
