c9042275af9d335b7e2ad60215c2fa8d5fa7f5bb
echocslsk <CardioLeo@gmail.com>
2025-05-21 23:54:24 -0600

    adds new form, loggedInForm; as yet no real changes made to it, though; way too sleepy

---
+++ 17-Database-Driven-Login-Form-Project/02-create-project-with-form/LoginForm/loggedInForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace LoginForm
+{
+    partial class loggedInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // loggedInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "loggedInForm";
+            this.Text = "You Got In!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
