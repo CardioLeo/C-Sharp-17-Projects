f5d7123f22a5c53f21c1c11615cc45a14a24838f
echocslsk <CardioLeo@gmail.com>
2025-05-26 17:26:31 -0600

    makes it open centered; fixed, not resizable; gets rid of maximize box

--- 17-Database-Driven-Login-Form-Project/02-create-project-with-form/LoginForm/loggedInForm.Designer.cs
+++ 17-Database-Driven-Login-Form-Project/02-create-project-with-form/LoginForm/loggedInForm.Designer.cs
@@ -35,7 +35,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "loggedInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "You Got In!";
             this.ResumeLayout(false);
 
