2b53866765b91dde2d6a3ff752ae2cc265985282
echocslsk <CardioLeo@gmail.com>
2025-05-27 00:10:29 -0600

    with slight variations, adds the code he writes in the final video of the project section that makes the login work when you press the login button; this is tested, and it works with correct password and username, and doesn't work when you type in the wrong stuff - so that's pretty cool

--- 17-Database-Driven-Login-Form-Project/06-create-a-database-and-a-table/LoginForm/Form1.cs
+++ 17-Database-Driven-Login-Form-Project/06-create-a-database-and-a-table/LoginForm/Form1.cs
@@ -20,8 +20,24 @@ namespace LoginForm
 
         private void logInButton_Click(object sender, EventArgs e)
         {
-            SqlConnection sqlcon = new SqlConnection(@"Data Source=GLASSMOUND\SQLSERVER2022;Initial Catalog=userLogin;Integrated Security=True;Trust Server Certificate=True");
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=GLASSMOUND\SQLSERVER2022;Initial Catalog=userLogin;Integrated Security=True;");
             string query = "SELECT * FROM logins WHERE username ='"+usernameTextBox.Text.Trim()+"'AND password ='"+passwordTextBox.Text.Trim()+"'"; // he did not capitalize the 'select'
+
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if (dtbl.Rows.Count == 1)
+            {
+                loggedInForm objloggedInForm = new loggedInForm();
+                this.Hide();
+                objloggedInForm.Show();
+                // hides main form if logged in
+                // then shows second form
+            }
+            else
+            {
+                MessageBox.Show("Please check your username and password");
+            }
         }
     }
 }
